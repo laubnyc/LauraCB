a30a39a0dee0e7500030c485b97ec6388e7e4ad5
Laura Buchanan <laub@Lauras-MacBook-Pro.local>
2019-09-25 18:41:40 -0500

    checkpoint

--- website/apps/homepage/urls.py
+++ website/apps/homepage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [
+app_name = 'homepage'
 
+urlpatterns = [
+    path('', views.index, name='index'),
 ]
--- website/apps/homepage/views.py
+++ website/apps/homepage/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+# def index(request):
+#     return render(request, 'index.html')
+
+def index(request):
+	return HttpResponse("Did this work?")
